8f63db3e8d422e33bff5068b23b415bdd3a6b288
natsukium <tm.tomatoes@gmail.com>
2017-11-17 16:53:42 +0900

    Fix bug on zsh

--- setup.sh
+++ setup.sh
@@ -2,18 +2,21 @@
 # If you want to use tensorflow-gpu on arpeggio, run this script!
 
 cd ${HOME}
+
+shell=`echo $SHELL | awk -F / '{print $NF}'`
+
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo '# pyenv setting' >> ~/.bashrc
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-source ~/.bashrc
+echo '# pyenv setting' >> ~/.${shell}rc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.${shell}rc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.${shell}rc
+echo 'eval "$(pyenv init -)"' >> ~/.${shell}rc
+source ~/.${shell}rc
 
 pyenv install $1
 pyenv local $1
 
-echo '# cuda setting' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH="/usr/local/cuda-8.0/lib64"' >> ~/.bashrc
-source ~/.bashrc
+echo '# cuda setting' >> ~/.${shell}rc
+echo 'export LD_LIBRARY_PATH="/usr/local/cuda-8.0/lib64"' >> ~/.${shell}rc
+source ~/.${shell}rc
 
 pip install tensorflow-gpu
